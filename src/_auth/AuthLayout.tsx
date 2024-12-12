1e911dd1d75bebe678452e974a228f3c7a0ed114
Sachin kumawat <sachinkumawat55@gmail.com>
2024-12-12 12:39:50 +0530

    initial register page

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,25 +1,25 @@
 import { Navigate, Outlet } from "react-router-dom";
 
 const AuthLayout = () => {
-  const isAuthenticate = false;
+  const isAuthenticated = false;
 
   return (
     <>
-      {isAuthenticate ? (
+      {isAuthenticated ? (
         <Navigate to="/" />
       ) : (
-        <section className="max-w-11/12 mx-auto  h-screen flex justify-center items-end relative">
-          <img
-            src="/assets/images/signupImage.svg"
-            alt="Background"
-            className="absolute inset-0 w-full h-full object-cover p-1 md:p-0 md:object-fill z-0"
-          />
-          <div className="relative z-10 w-full flex justify-center items-end">
-            {/* <div className="w-full flex justify-center items-center"> */}
-            <Outlet />
-            {/* </div> */}
-          </div>
-        </section>
+        <>
+          <section className="max-w-11/12 mx-auto  h-screen flex justify-center items-end relative">
+            <img
+              src="/assets/images/signupImage.svg"
+              alt="Background"
+              className="absolute inset-0 w-full h-full object-cover p-1 md:p-0 md:object-fill z-0"
+            />
+            <div className="relative z-10 w-full flex justify-center items-end">
+              <Outlet />
+            </div>
+          </section>
+        </>
       )}
     </>
   );
--- src/_auth/register/Signup.tsx
+++ src/_auth/register/Signup.tsx
@@ -1,12 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { FaGoogle } from "react-icons/fa";
+
 const Signup = () => {
   return (
-    <div className="bg-white w-full gap-5  md:w-6/12 lg:w-8/12 px-5 pt-5 pb-20 flex justify-center items-center flex-col rounded-t-[50px] ">
+    <div className="bg-white w-full gap-5  md:w-6/12 lg:w-8/12 px-5 pt-10 pb-20 flex justify-center items-center flex-col rounded-t-[50px] ">
       <div className="flex gap-2">
         <img src="/assets/images/logo.svg" alt="logo" width={50} height={50} />
         <h1 className="text-2xl">Vibesnap</h1>
       </div>
       <p>Moments That Matter, Shared Forever.</p>
-      <button>click</button>
+      <Button>
+        <FaGoogle />
+        Continue with Google
+      </Button>
     </div>
   );
 };
