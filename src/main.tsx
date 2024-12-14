d557822a1239c0a0737b1886f3303614acf101ee
Sachin kumawat <sachinkumawat55@gmail.com>
2024-12-14 21:41:00 +0530

    index.html updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.tsx";
@@ -5,11 +6,13 @@ import QueryProvider from "./lib/react-query/QueryProvider.tsx";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <QueryProvider>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </QueryProvider>
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <QueryProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </QueryProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
